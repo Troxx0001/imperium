2734605b45f4f7cc6a77f46850947831d140d1e6
Diego <80925024+Troxx0001@users.noreply.github.com>
2025-06-04 11:01:05 -0300

    style: improve site appearance

--- static/js/loja.js
+++ static/js/loja.js
@@ -1,1 +1,8 @@
-// This file is intentionally left blank.
+document.addEventListener('DOMContentLoaded', function () {
+  const flashes = document.querySelectorAll('.flash-message');
+  if (flashes.length) {
+    setTimeout(() => {
+      flashes.forEach(el => el.style.display = 'none');
+    }, 3000);
+  }
+});
