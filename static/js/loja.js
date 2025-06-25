acbec8f8bc5f8ceea8db6e31f2f6c7068f9a324d
Diego <80925024+Troxx0001@users.noreply.github.com>
2025-06-25 00:19:50 -0300

    Improve site design with animations

--- static/js/loja.js
+++ static/js/loja.js
@@ -2,7 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
   const flashes = document.querySelectorAll('.flash-message');
   if (flashes.length) {
     setTimeout(() => {
-      flashes.forEach(el => el.style.display = 'none');
+      flashes.forEach(el => {
+        el.classList.add('fade-out');
+        setTimeout(() => el.remove(), 500);
+      });
     }, 3000);
   }
+
+  // reveal product cards with a staggered effect
+  const produtos = document.querySelectorAll('.produto');
+  produtos.forEach((card, idx) => {
+    setTimeout(() => card.classList.add('reveal'), idx * 100);
+  });
 });
