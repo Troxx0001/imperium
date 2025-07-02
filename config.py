f7d477346854aa41e574154d275eeb4fa84cf40d
diego <diego.dos248@gmail.com>
2025-07-02 15:38:14 -0300

    Removendo config.py do repositório e adicionando ao .gitignore

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'sua-chave-secreta')
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'SQLALCHEMY_DATABASE_URI',
-        'postgresql://postgres:5Phwwm1iIaD4@localhost:5432/imperium_utf8',
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
